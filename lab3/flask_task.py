f6d24c2f3a78ede3148e6039296d50427072eb9c
Tony <tonywang3334@gmail.com>
2017-09-24 13:31:34 +0200

    added time to while loop and fixed boolean

--- lab3/flask_task.py
+++ lab3/flask_task.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import subprocess
 import sys
+import time
 from tasks import count_pronouns
 app = Flask(__name__)
 
@@ -11,8 +12,8 @@ def index():
 @app.route('/count_pronouns', methods=['GET'])
 def count():
 	result = count_pronouns.delay()
-	while not result.ready():
-		pass
+	while result.ready():
+		time.sleep(0.1)
 	return result.result
 
 if __name__ == '__main__':
