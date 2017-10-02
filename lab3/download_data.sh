e94fe1b71686aa08fff7e12dc6382e911ad8c9c5
Tony Wang <towa4489@arrhenius.it.uu.se>
2017-10-02 11:18:37 +0200

    file for downloading lab3 data from simon

---
+++ lab3/download_data.sh
@@ -0,0 +1,3 @@
+sudo wget https://raw.githubusercontent.com/simonljus/cloudlab3/master/data/data01
+sudo wget https://raw.githubusercontent.com/simonljus/cloudlab3/master/data/data02
+sudo wget https://raw.githubusercontent.com/simonljus/cloudlab3/master/data/data03
